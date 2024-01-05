b93b0aa82f8f6055f3c1a4aee100a22416202b3a
manuelxantony <manuelxantony@gmail.com>
2024-01-05 20:08:04 +0530

    car will be fixed at 70% of the canvas/ road.

--- ui/main.js
+++ ui/main.js
@@ -14,8 +14,16 @@ animate();
 function animate() {
   car.update();
   canvas.height = window.innerHeight;
+
+  ctx.save();
+  // for keeping car at 70% of canvas and to move road
+  ctx.translate(0, -car.y + canvas.height * 0.7);
+
   road.draw(ctx);
   car.draw(ctx);
+
+  ctx.restore();
+
   // requests the browser to call a user-supplied callback function before the next repaint.
   requestAnimationFrame(animate); // this function registers animate function and run it
 }
